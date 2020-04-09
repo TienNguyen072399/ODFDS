45e8588b8c830718c5e5ceb3dfe2a4d9bade21ad
Tien-PC <Tien.Nguyen072399@gmail.com>
2020-04-08 18:50:14 -0700

    Added header bar

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../pages/TempCSS.css";
+
+class Header extends Component {
+  state = {};
+
+  render() {
+    return <h1 className="header">FLY ME FOOD</h1>;
+  }
+}
+
+export default Header;
