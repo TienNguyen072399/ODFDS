78ad16e016db9f7909bec9ffa38a892927c3137f
minhngo90 <45471628+minhngo90@users.noreply.github.com>
2020-04-21 04:27:23 -0700

    Update DriverMap.js

--- client/src/pages/DriverMap.js
+++ client/src/pages/DriverMap.js
@@ -56,28 +56,7 @@ class DriverMap extends Component {
     };
   }
 
-  getStartCoordinates = (location) => {
-    // get location from fetching mapbox api 
-
-    //console.log(location);
-    //let coordinates;
-    
-    var endpoint = 'mapbox.places';
-    var search_text = location.split(" ").join('%20');
-    const MAP_API = 'https://api.mapbox.com/geocoding/v5/';
-    const QUERY = endpoint+'/'+search_text+'.json';
-    const KEY = '?country=US&access_token=pk.eyJ1IjoibmdvdGhhb21pbmg5MCIsImEiOiJjazkwdnVhdmIwNXAyM2xvNmd0MnFsdXJlIn0.mT75xgKIwKFgt8BdWGouCg';
-    //console.log(MAP_API + QUERY + KEY);
-    
-    fetch(MAP_API + QUERY + KEY).then((response) => response.json())
-      .then(data => {
-        this.setState(() => ({coordinates: data.features[0].geometry.coordinates}))
-        
-      })
-      console.log(this.state.coordinates);
-      return " Coordinates: " + this.state.coordinates[0]+" , "+this.state.coordinates[1]
-  };
-  
+   
   render() {
       return (
       <div 
